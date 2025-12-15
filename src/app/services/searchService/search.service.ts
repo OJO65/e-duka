68e144f8c6d5e532a137a405cc43be05034f929f
Ochieng Jeffrey Odhiambo <129600311+OJO65@users.noreply.github.com>
2025-12-15 13:38:24 +0300

    Add SearchService with BehaviorSubject and unit test
    - Created `SearchService` to manage global product search state using RxJS BehaviorSubject.
    - Exposed `search$` observable for components to subscribe to search query changes.
    - Implemented `setSearch(query: string)` to update the current search term.
    - Added basic unit test to ensure the service is instantiated correctly.

---
+++ src/app/services/searchService/search.service.ts
@@ -0,0 +1,13 @@
+import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
+
+
+@Injectable({ providedIn: 'root' })
+export class SearchService {
+  private searchSubject = new BehaviorSubject<string>('');
+  search$ = this.searchSubject.asObservable();
+
+  setSearch(query: string) {
+    this.searchSubject.next(query);
+  }
+}
