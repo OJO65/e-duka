c3735c33b6727d568dd9011ecb6c47f336ad0b92
Ochieng Jeffrey Odhiambo <129600311+OJO65@users.noreply.github.com>
2025-12-19 18:26:39 +0300

    feat(router): add product detail route with dynamic productId

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,11 +2,14 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { ShopComponent } from './pages/shop/shop.component';
 import { CartComponent } from './pages/cart/cart.component';
+import { ProductDetailComponent } from './pages/product-detail/product-detail.component';
 
 
 export const routes: Routes = [
     {path: '', redirectTo: 'home', pathMatch: 'full' },
     {path: 'home', component: HomeComponent},
     {path: 'shop', component: ShopComponent},
-    {path: 'cart', component: CartComponent}
+    {path: 'cart', component: CartComponent},
+    {path: 'product/:productId', component: ProductDetailComponent},
+    {path: '**', redirectTo: 'home' }
 ];
