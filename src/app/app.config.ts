75abf05d563982d7dacd877d735eb0405346bf3b
Ochieng Jeffrey Odhiambo <129600311+OJO65@users.noreply.github.com>
2026-05-25 11:15:07 +0300

    update application configuration setup

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,30 +1,15 @@
 import {
   ApplicationConfig,
   provideZoneChangeDetection,
-  inject,
 } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
+import { provideHttpClient, withFetch } from '@angular/common/http';
 import { routes } from './app.routes';
-import { provideHttpClient } from '@angular/common/http';
-import { provideApollo } from 'apollo-angular';
-import { HttpLink } from 'apollo-angular/http';
-import { InMemoryCache } from '@apollo/client';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
-    provideHttpClient(),
-    provideApollo(() => {
-      const httpLink = inject(HttpLink);
-
-      return {
-        link: httpLink.create({
-          uri: 'https://mock.shop/api',
-        }),
-        cache: new InMemoryCache(),
-      };
-    }),
+    provideHttpClient(withFetch()),
   ],
 };
