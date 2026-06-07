7cb1403381bfc441176920a1f7566229cac3ea6a
Ochieng Jeffrey Odhiambo <129600311+OJO65@users.noreply.github.com>
2026-06-07 18:48:46 +0300

    fix: restore Netlify compatible server.ts

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,8 @@
-import { mergeApplicationConfig, ApplicationConfig } from '@angular/core';
-import { provideServerRendering } from '@angular/platform-server';
-import { provideClientHydration } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
+import { CommonEngine } from '@angular/ssr/node';
+import { render } from '@netlify/angular-runtime/common-engine.js';
 
-const serverConfig: ApplicationConfig = {
-  providers: [
-    provideServerRendering(),
-    provideClientHydration(),
-  ]
-};
+const commonEngine = new CommonEngine();
 
-export const config = mergeApplicationConfig(appConfig, serverConfig);
+export async function netlifyCommonEngineHandler(request: Request, context: any): Promise<Response> {
+  return await render(commonEngine);
+}
