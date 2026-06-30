3a5de0a0b9c27d4e5d54e2b14360ca2afb49ad5d
Ochieng Jeffrey Odhiambo <129600311+OJO65@users.noreply.github.com>
2026-06-30 10:11:12 +0300

    feat: add scroll reveal animations to careers page

--- src/app/pages/careers/careers.component.ts
+++ src/app/pages/careers/careers.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ScrollRevealDirective } from '../../directives/scroll-reveal.directive';
 
 @Component({
   selector: 'app-careers',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, ScrollRevealDirective],
   templateUrl: './careers.component.html',
   styleUrls: ['./careers.component.css']
 })
