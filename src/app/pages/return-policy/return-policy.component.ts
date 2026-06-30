68714bcf5b7313f52677fe8bd35f150b590c5666
Ochieng Jeffrey Odhiambo <129600311+OJO65@users.noreply.github.com>
2026-06-30 09:47:31 +0300

    feat: add scroll reveal animations to return policy page

--- src/app/pages/return-policy/return-policy.component.ts
+++ src/app/pages/return-policy/return-policy.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ScrollRevealDirective } from '../../directives/scroll-reveal.directive';
 
 @Component({
   selector: 'app-return-policy',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, ScrollRevealDirective],
   templateUrl: './return-policy.component.html',
   styleUrls: ['./return-policy.component.css'],
 })
